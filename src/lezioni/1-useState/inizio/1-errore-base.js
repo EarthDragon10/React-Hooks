c4267e8633a024437d48616d355296ab8f8cf835
earthdragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-10-10 11:08:39 +0200

    Primo Commit

--- src/lezioni/1-useState/inizio/1-errore-base.js
+++ src/lezioni/1-useState/inizio/1-errore-base.js
@@ -2,7 +2,7 @@ import React from "react";
 //Far vedere che il componente non si riaggiorna ma la variabile si
 //Spiegare che cosa è un React Fragment e che è possibile abbreviarlo
 const ErroreBase = () => {
-  return <div>Error Components</div>;
+  return <div>Error Component</div>;
 };
 
 export default ErroreBase;
