6cd2a1389ba6caf8d9ff857b1d871534c6c1fedf
EarthDragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-11-27 16:13:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Inizio from "./lezioni/1-useState/inizio/2-useState-basi";
+import Inizio from "./lezioni/2-useEffect/inizio/1-useEffect-basi";
 
 // Progetto per ripassare React
 
 function App() {
-	return (
-		<div className="App">
-			<section className="container text-center my-3">
-				<Inizio />
-			</section>
-		</div>
-	);
+  return (
+    <div className="App">
+      <section className="container text-center my-3">
+        <Inizio />
+      </section>
+    </div>
+  );
 }
 
 export default App;
