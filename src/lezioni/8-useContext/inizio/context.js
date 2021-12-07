ffc6be34d860d46e0605d019e67369c3ffaa9738
EarthDragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-12-07 12:04:49 +0100

    feat: added hook useContext

--- src/lezioni/8-useContext/inizio/context.js
+++ src/lezioni/8-useContext/inizio/context.js
@@ -1,31 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { data } from "../../../data";
+
+const AppContext = React.createContext();
 
 const MainComponent = () => {
   const [people, setPeople] = useState(data);
 
   const removePeople = (id) => setPeople(people.filter((el) => el.id !== id));
   return (
-    <div>
-      <h3>Passaggio di Proprietà a cascata </h3>
-      <Elenco people={people} removePeople={removePeople} />
-    </div>
+    <AppContext.Provider value={{ people, removePeople }}>
+      <div>
+        <h3>useContext </h3>
+        <Elenco people={people} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
-const Elenco = ({ people, removePeople }) => {
+const Elenco = () => {
+  const { people } = useContext(AppContext);
+  // console.log(info);
   return (
     <div>
       {people.map((el, index) => {
-        return <Persona key={index} {...el} removePeople={removePeople} />;
+        return <Persona key={index} {...el} />;
       })}
     </div>
   );
 };
 
-const Persona = ({ id, name, removePeople }) => {
+const Persona = ({ id, name }) => {
+  const { removePeople } = useContext(AppContext);
+  console.log();
+
   return (
-    <div className="item">
+    <div className="item shadow">
       <h5> {name} </h5>
       <button className="button delete-button" onClick={() => removePeople(id)}>
         {" "}
