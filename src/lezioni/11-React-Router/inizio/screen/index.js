8bfe373e747e2925dd1a71023d898ccc25a290db
EarthDragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-12-07 17:46:37 +0100

    feat: added router, route and switch components

--- src/lezioni/11-React-Router/inizio/screen/index.js
+++ src/lezioni/11-React-Router/inizio/screen/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
 const index = () => {
-  return <div>React Router</div>;
+  return (
+    <Router>
+      React Router
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default index;
