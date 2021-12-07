c40ac057ce84047dad5f21f7b6b3a3511810d28d
EarthDragon10 <74973560+EarthDragon10@users.noreply.github.com>
2021-12-07 19:21:32 +0100

    feat: added single page template

---
+++ src/lezioni/11-React-Router/inizio/screen/SingleProgetto.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SingleProgetto = (props) => {
+  console.log(props);
+  return <div>Singolo Progetto</div>;
+};
+
+export default SingleProgetto;
--- src/lezioni/11-React-Router/inizio/screen/index.js
+++ src/lezioni/11-React-Router/inizio/screen/index.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import ErrorPage from "./ErrorPage";
+import Progetti from "./Progetti";
+import SingleProgetto from "./SingleProgetto";
 
 const index = () => {
   return (
     <Router>
-      React Router
+      <ul className="nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/progetti" className="nav-link">
+            Progetti
+          </Link>
+        </li>
+      </ul>
+
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/progetti" component={Progetti} />
+        <Route path="/progetto/:id" component={SingleProgetto} />
+        <Route path="*" component={ErrorPage} />
       </Switch>
     </Router>
   );
