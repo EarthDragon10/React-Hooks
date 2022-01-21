ab08b6b03a7b813ea2fa8d9d66d35c7e374e768e
Laguda Stefano <lagudadev@gmail.com>
2022-01-21 17:51:20 +0100

    esempio di useState con oggetto + spread

--- src/lezioni/1-useState/inizio/4-useState.object.js
+++ src/lezioni/1-useState/inizio/4-useState.object.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateObject = () => {
-  return <div>Object Componente UseState</div>;
+  const [person, setPerson] = useState({
+    name: "Stefano",
+    age: 29,
+    hello: "Ciao",
+  });
+
+  const changeHello = () => {
+    setPerson({ ...person, age: 30, hello: "Ciao, oggi ho fatto gli anni" });
+  };
+
+  return (
+    <div className="item shadow ">
+      <div className="text-left">
+        <h5>{person.name}</h5>
+        <h5>{person.age}</h5>
+        <h6>{person.hello}</h6>
+      </div>
+      <button type="button" className="button" onClick={changeHello}>
+        Cambia il Saluto
+      </button>
+    </div>
+  );
 };
 
 export default StateObject;
