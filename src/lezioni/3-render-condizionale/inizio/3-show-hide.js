c36ac56efb797e9766eb0acf6428b417672a429e
Laguda Stefano <lagudadev@gmail.com>
2022-01-25 12:20:31 +0100

    esempio di come mostrare e nascondere un elemento ed ottimizzandola con la clean-up function

--- src/lezioni/3-render-condizionale/inizio/3-show-hide.js
+++ src/lezioni/3-render-condizionale/inizio/3-show-hide.js
@@ -1,9 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const HideorShowComponent = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="card shadow mt-5 p-5">
+      <button
+        className="button w-50 col-6 offset-3 shadow"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        {show ? "Nascondi" : "Mostra"}
+      </button>
+      {show && <Element />}
+      {/* {show ? <Element /> : null} */}
+    </div>
+  );
+};
+
+const Element = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCounter((prevValue) => prevValue + 1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [counter]);
+
   return (
     <div>
-      <h1>Show or Hide Component</h1>
+      <h2>{counter}</h2>
     </div>
   );
 };
