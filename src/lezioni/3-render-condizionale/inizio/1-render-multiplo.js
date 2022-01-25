baaa5094f62ca26b335eda07e10167759737acae
Laguda Stefano <lagudadev@gmail.com>
2022-01-25 09:44:12 +0100

    feat: esemio di loading, rendering e fetchData condizionale

--- src/lezioni/3-render-condizionale/inizio/1-render-multiplo.js
+++ src/lezioni/3-render-condizionale/inizio/1-render-multiplo.js
@@ -1,17 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 const url = "https://api.github.com/users/QuincyLarson";
 
 const ConditionalCompining = () => {
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
-  if (loading) {
-    return <h2>Loading...</h2>;
+  const fetchData = async () => {
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const response = await Axios.get(url);
+
+      console.log(response);
+
+      const { login } = response.data;
+
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  } else if (isError) {
+    return <Error />;
+  } else {
+    return (
+      <article className="card d-flex align-items-center p-5 shadow mt-5">
+        <img
+          src={user.avatar_url}
+          alt={user.name}
+          style={{ width: "80px", height: "80px" }}
+          className="rounded-circle my-4 shadow"
+        />
+        <h2>{user.login}</h2>
+      </article>
+    );
   }
+};
 
+const Loading = () => {
   return (
     <div>
-      <h2>Conditional Rendering</h2>
+      <h2>Loading..</h2>
+    </div>
+  );
+};
+
+const Error = () => {
+  return (
+    <div>
+      <h2>Error...</h2>
     </div>
   );
 };
