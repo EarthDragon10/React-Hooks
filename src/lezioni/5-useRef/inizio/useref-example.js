b645ee9a0a1950948a57b8de6021ddd06a58087e
Laguda Stefano <lagudadev@gmail.com>
2022-02-03 11:38:55 +0100

    feat: added useRef chapter

--- src/lezioni/5-useRef/inizio/useref-example.js
+++ src/lezioni/5-useRef/inizio/useref-example.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const RefExample = () => {
+  const inputRef = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+  };
+
   return (
     <div>
-      <h2>Use RefContainer</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+        <button className="button" type="submit">
+          Invia
+        </button>
+      </form>
     </div>
   );
 };
